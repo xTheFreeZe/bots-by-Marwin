4a011cf7ccfc8de017b5eed53fb6758a3ddbe037
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-10-16 11:41:58 +0200

    :star:

---
+++ Plans.md
@@ -0,0 +1,17 @@
+Plan 1:
+A Discord Bot
+A Welcome Message
+3 Custom Commands
+Free Hosting
+
+Plan 2:
+Everything from plan 1
++5 Custom Commands
+A poll command
+80% Uptime
+
+
+Plan 3
+Everything from plan 2
+Unlimited custom commands
+99% Uptime
---
+++ js/index.js
@@ -0,0 +1,112 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+let usernames = [
+
+];
+
+let passwords = [
+
+];
+
+function Submitfunction() {
+
+    //If you are actually checking out the code behind this... Screw you!!!
+
+    let Username = document.getElementById("SubmitName").value;
+    let Password = document.getElementById("SubmitPassword").value;
+
+    if (!Username) {
+        return document.getElementById("alert_messages").innerHTML = "Please submit a name!";
+    }
+
+    if (!Password) {
+        return document.getElementById("alert_messages").innerHTML = "Please submit a password!";
+    }
+
+    /*
+    if (!passwords.includes(Password)) {
+
+        return document.getElementById("alert_messages").innerHTML = "Please register first!";
+
+    }
+    */
+
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("username", Username);
+        localStorage.setItem("password", Password);
+    } else {
+        document.getElementById("alert_messages").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+
+    window.location.href = "options.html";
+
+    //If you are actually checking out the code behind this... Screw you!!!
+
+}
+
+function RegsiterFunction() {
+
+    let Username = document.getElementById("SubmitName").value;
+    let Password = document.getElementById("SubmitPassword").value;
+
+    if (!Username) {
+        return document.getElementById("alert_messages").innerHTML = "Please submit a name!";
+    }
+
+    if (!Password) {
+        return document.getElementById("alert_messages").innerHTML = "Please submit a password!";
+    }
+
+
+    const CheckUserName = localStorage.getItem('username');
+    const CheckPassWord = localStorage.getItem('password');
+
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("username", Username);
+        localStorage.setItem("password", Password);
+    } else {
+        document.getElementById("alert_messages").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+
+    document.getElementById("alert_messages").innerHTML = "Successfully created an Account!";
+
+
+    window.location.href = "options.html";
+
+
+}
+
+window.onload = function GetCurrentAccount() {
+
+    if (!document.getElementById("CurrentAccount")) return;
+
+    if (localStorage.getItem("username")) {
+
+        document.getElementById("CurrentAccount").innerHTML = localStorage.getItem("username");
+
+    } else {
+
+        document.getElementById("CurrentAccount").innerHTML = ("Not logged in yet!");
+
+    }
+
+};
+
+function LogOut() {
+
+    if (!localStorage.getItem("username")) {
+
+        alert('Please log in before you log out...');
+        return;
+    }
+
+    localStorage.clear();
+
+    document.getElementById("CurrentAccount").innerHTML = ("Succesfully logged out!");
+
+}
